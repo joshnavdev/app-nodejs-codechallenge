6e034c77c3eaa9cb141006126efd28eb3d35e5a8
Joshua Navarro <joshua.navarro35@gmail.com>
2025-08-10 05:00:27 -0500

    feat: add unit tests for anti-fraud service

---
+++ anti-fraud-service/src/anti-fraud/anti-fraud.controller.spec.ts
@@ -0,0 +1,33 @@
+import { AntiFraudService } from './anti-fraud.service';
+import TransactionDto from './dtos/transaction.dto';
+import { AntiFraudController } from './anti-fraud.controller';
+import { Test } from '@nestjs/testing';
+
+describe('AntiFraudController', () => {
+  let controller: AntiFraudController;
+  let mockAntiFraudService: jest.Mocked<AntiFraudService>;
+
+  beforeEach(async () => {
+    mockAntiFraudService = {
+      validateTransaction: jest.fn(),
+      emitTransactionStatusUpdate: jest.fn(),
+    } as unknown as jest.Mocked<AntiFraudService>;
+    const module = await Test.createTestingModule({
+      controllers: [AntiFraudController],
+      providers: [{ provide: AntiFraudService, useValue: mockAntiFraudService }],
+    }).compile();
+    controller = module.get<AntiFraudController>(AntiFraudController);
+  });
+
+  describe('transactionCreated', () => {
+    it('should validate transaction and emit status update', () => {
+      const transaction: TransactionDto = new TransactionDto('trx-id-01', 1200);
+
+      mockAntiFraudService.validateTransaction.mockReturnValue(true);
+
+      controller.transactionCreated(transaction);
+      expect(mockAntiFraudService.validateTransaction).toHaveBeenCalledWith(transaction);
+      expect(mockAntiFraudService.emitTransactionStatusUpdate).toHaveBeenCalledWith(transaction.id, true);
+    });
+  });
+});
---
+++ anti-fraud-service/src/anti-fraud/anti-fraud.service.spec.ts
@@ -0,0 +1,58 @@
+import { AntiFraudService } from './anti-fraud.service';
+import { ClientKafka } from '@nestjs/microservices';
+import { Test, TestingModule } from '@nestjs/testing';
+import TransactionDto from './dtos/transaction.dto';
+import {
+  APPROVE_TRANSACTION_TOPIC,
+  REJECT_TRANSACTION_TOPIC,
+  TRANSACTION_AMOUNT_THRESHOLD,
+  ANTI_FRAUD_EVENT_PRODUCER,
+} from './constants';
+
+describe('AntiFraudService', () => {
+  let service: AntiFraudService;
+  let mockClientProducer: ClientKafka;
+
+  beforeEach(async () => {
+    mockClientProducer = {
+      emit: jest.fn(),
+    } as unknown as ClientKafka;
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AntiFraudService,
+        {
+          provide: ANTI_FRAUD_EVENT_PRODUCER,
+          useValue: mockClientProducer,
+        },
+      ],
+    }).compile();
+
+    service = module.get<AntiFraudService>(AntiFraudService);
+  });
+
+  describe('validateTransaction', () => {
+    it('should return true for transactions above the threshold', () => {
+      const dto: TransactionDto = { id: 'test', amount: TRANSACTION_AMOUNT_THRESHOLD + 1 };
+      expect(service.validateTransaction(dto)).toBe(true);
+    });
+
+    it('should return false for transactions below the threshold', () => {
+      const dto: TransactionDto = { id: 'test', amount: TRANSACTION_AMOUNT_THRESHOLD - 1 };
+      expect(service.validateTransaction(dto)).toBe(false);
+    });
+  });
+
+  describe('emitTransactionStatusUpdate', () => {
+    it('should emit reject transaction event for fraudulent transactions', () => {
+      const trxId = 'fraudulent-transaction';
+      service.emitTransactionStatusUpdate(trxId, true);
+      expect(mockClientProducer.emit).toHaveBeenCalledWith(REJECT_TRANSACTION_TOPIC, { transactionId: trxId });
+    });
+
+    it('should emit approve transaction event for non-fraudulent transactions', () => {
+      const trxId = 'valid-transaction';
+      service.emitTransactionStatusUpdate(trxId, false);
+      expect(mockClientProducer.emit).toHaveBeenCalledWith(APPROVE_TRANSACTION_TOPIC, { transactionId: trxId });
+    });
+  });
+});
